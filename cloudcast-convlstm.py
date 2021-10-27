29c1a8c4e1b24daade9cc36f7900a2d4302277ee
Mikko Partio <mikko.partio@fmi.fi>
2021-10-27 11:32:49 +0300

    Update convlstm startup file to match unet

--- cloudcast-convlstm.py
+++ cloudcast-convlstm.py
@@ -1,26 +1,25 @@
 import numpy as np
-import glob
 import matplotlib.pyplot as plt
 import sys
-import argparser
-
+import argparse
+import datetime
 from tensorflow.keras.models import save_model # import datasets, models, layers
-from PIL import Image
 from model import *
+from preprocess import *
 
-TRAIN_SERIES_LENGTH = 10
-
-START_DATE = '20201001T0000'
-STOP_DATE = '20210331T2345'
-
-PREPROCESS = True
+TIMESERIES_LENGTH = 10
 
 def parse_command_line():
-    parser = argparser.ArgumentParser()
+    parser = argparse.ArgumentParser()
     parser.add_argument("--img_size", action='store', type=str, required=True)
+    parser.add_argument("--start_date", action='store', type=str, required=True)
+    parser.add_argument("--stop_date", action='store', type=str, required=True)
+
+    args = parser.parse_args()
 
     args.img_size = tuple(map(int, args.img_size.split('x')))
-
+    args.start_date = datetime.datetime.strptime(args.start_date, '%Y-%m-%d')
+    args.stop_date = datetime.datetime.strptime(args.stop_date, '%Y-%m-%d')
 
     return args
 
@@ -70,29 +69,47 @@ def show_examples():
     print(f"Displaying frames for example {data_choice}.")
     plt.show()
 
-args = parse_command_line()
 
-dataset = create_dataset()
+def fit(m, args):
+    batch_size = 8
 
-# show_examples(dataset)
+    train_gen, val_gen = create_generators(args.start_date, args.stop_date, n_channels=TIMESERIES_LENGTH, batch_size=batch_size, img_size=args.img_size, output_is_timeseries=True)
 
-x_train, y_train, x_val, y_val = create_train_val_split(dataset)
+    print("Number of train dataset elements: {}".format(len(train_gen.dataset)))
+    print("Number of validation dataset elements: {}".format(len(val_gen.dataset)))
 
-print("train dataset size: {str(x_train)}")
-
-m = convlstm(input_size=args.img_size + (1,))
-
-cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath='checkpoints/convlstm_{}_{}x{}_{}/cp.ckpt'.format(LOSS_FUNCTION, args.img_size[0], args.img_size[1], TRAIN_SERIES_LENGTH),
+    cp_cb = tf.keras.callbacks.ModelCheckpoint(filepath='checkpoints/convlstm_{}_{}x{}_{}/cp.ckpt'.format(LOSS_FUNCTION, args.img_size[0], args.img_size[1], TIMESERIES_LENGTH),
                                                  save_weights_only=True,
                                                  verbose=1)
 
-# Define some callbacks to improve training.
-early_stopping_callback = keras.callbacks.EarlyStopping(monitor="val_loss", patience=10)
-reduce_lr_callback = keras.callbacks.ReduceLROnPlateau(monitor="val_loss", patience=5)
+    early_stopping_cb = keras.callbacks.EarlyStopping(monitor="val_loss", patience=10)
+    reduce_lr_cb = keras.callbacks.ReduceLROnPlateau(monitor="val_loss", patience=5)
 
-hist = m.fit(x_train, y_train, epochs=20, batch_size=3, validation_data=(x_val, y_val), callbacks=[cp_callback, early_stopping_callback, reduce_lr_callback])
+#    hist = m.fit(x_train, y_train, epochs=20, batch_size=3, validation_data=(x_val, y_val), callbacks=[cp_callback, early_stopping_callback, reduce_lr_callback])
 
-print(hist)
+    hist = m.fit(train_gen, epochs = 500, validation_data = val_gen, callbacks=[cp_cb, early_stopping_cb, reduce_lr_cb])
 
-save_model(m, 'models/convlstm_{}_{}x{}_{}/'.format(LOSS_FUNCTION, args.img_size[0], args.img_size[1], TRAIN_SERIES_LENGTH))
+    return hist
 
+
+def run_model(args):
+    model_dir = 'models/convlstm_{}_{}x{}_{}'.format(LOSS_FUNCTION, args.img_size[0], args.img_size[1], TIMESERIES_LENGTH)
+
+    m = convlstm(input_size=args.img_size + (1,))
+
+    start = datetime.datetime.now()
+
+    hist = fit(m, args)
+
+    duration = datetime.datetime.now() - start
+
+    save_model(m, model_dir)
+    plot_hist(hist, model_dir)
+
+    print(f"Model training finished in {duration}")
+
+
+if __name__ == "__main__":
+    args = parse_command_line()
+
+    run_model(args)
