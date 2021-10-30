29766b53ad501451d7e6db1e73f8780750821ab2
Mikko Partio <mikko.partio@fmi.fi>
2021-10-30 15:57:14 +0300

    Bugfixes

--- infer.py
+++ infer.py
@@ -10,6 +10,7 @@ from datetime import datetime, timedelta
 from sklearn.metrics import mean_absolute_error
 from fileutils import *
 from preprocess import *
+from plotutils import *
 
 CONVLSTM = False
 
@@ -57,7 +58,7 @@ def infer_many(seed, num_predictions):
         else:
             predictions.append(infer(seed))
 
-    return predictions
+    return np.asarray(predictions)
 
 
 
@@ -82,11 +83,10 @@ def read_images(times, series, preprocess_label):
 
 
 def predict_from_series(dataseries, num):
-    ret = []
     for _ in range(num):
         pred = m.predict(np.expand_dims(dataseries, axis=0))
         pred = np.squeeze(pred, axis=0)
-        predicted_frame = to_binary_mask(np.expand_dims(pred[-1, ...], axis=0))
+        predicted_frame = np.expand_dims(pred[-1, ...], axis=0)
 
 #        predicted_frame = sharpen(predicted_frame, 2)
 #        print(np.min(predicted_frame), np.mean(predicted_frame), np.max(predicted_frame))
@@ -94,97 +94,6 @@ def predict_from_series(dataseries, num):
         dataseries = np.concatenate((dataseries, predicted_frame), axis=0)
 
     return dataseries[-num:]
-
-
-def plot_convlstm(ground_truth, predictions, mnwc):
-    fig, axes = plt.subplots(4, ground_truth.shape[0], figsize=(16, 8), constrained_layout=True)
-
-    for idx, ax in enumerate(axes[0]):
-        ax.imshow(np.squeeze(ground_truth[idx]), cmap='gray_r')
-        ax.set_title(f'ground truth frame {idx}')
-        ax.axis('off')
-
-    for idx, ax in enumerate(axes[1]):
-        ax.imshow(np.squeeze(predictions[idx]), cmap='gray_r')
-        ax.set_title(f'prediction frame {idx}')
-        ax.axis('off')
-
-    for idx, ax in enumerate(axes[2]):
-        ax.imshow(np.squeeze(mnwc[idx]), cmap='gray_r')
-        ax.set_title(f'mnwc frame {idx}')
-        ax.axis('off')
-
-    for idx, ax in enumerate(axes[3]):
-        r = ax.imshow(np.squeeze(ground_truth[idx] - predictions[idx]), cmap='bwr')
-
-        if idx == 0:
-            plt.colorbar(r, ax=axes[3])
-        ax.set_title(f'diff frame {idx}')
-        ax.axis('off')
-
-    plt.show()
-
-
-def plot_mae(data, labels, step=timedelta(minutes=15)):
-    print(data)
-    print(labels)
-    assert(len(data) == len(labels))
-    fig = plt.figure()
-    ax = plt.axes()
-
-    x = list(map(lambda x: step * x, range(len(data[0]))))
-    x = list(map(lambda x: '{}m'.format(int(x.total_seconds() / 60)), x))
-
-    for i,mae in enumerate(data):
-        ax.plot(x, mae, label=labels[i])
-
-    plt.legend()
-    plt.title(f'mae over {len(data[0])} predictions')
-    plt.show()
-
-
-def plot_timeseries(datas, labels):
-    assert(len(datas) == len(labels))
-    fig, axes = plt.subplots(datas[0].shape[0], len(datas), figsize=(14, 8))
-
-    for i, row in enumerate(axes):
-        for j in range(len(datas)):
-            row[j].imshow(datas[j][i]*255, cmap='gray_r')
-#            r2 = row[2].imshow(diff, cmap='RdYlBu_r')
-#            plt.colorbar(r2, ax=row[2])
-
-            row[j].axis("off")
-
-            row[j].set_title(f"{labels[j]} {i*15} min")
-
-#        time = times[i]
-
-#        if i == 0:
-#            row[0].imshow(gt[0], cmap='gray_r')
-#            row[0].axis("off")
-#            row[0].set_title(f"Ground truth 0 min")
-#            row[1].axis("off")
-#            row[2].axis("off")
-
-#            continue
-
-#        diff = (gt[i] - cloudcast[i])
-
-#        row[0].imshow(gt[i]*255, cmap='gray_r')
-#        row[1].imshow(cloudcast[i]*255, cmap='gray')
-#        r2 = row[2].imshow(diff, cmap='RdYlBu_r')
-#        plt.colorbar(r2, ax=row[2])
-
-#        row[0].axis("off")
-#        row[1].axis("off")
-#        row[2].axis("off")
-
-#        row[0].set_title(f"Ground truth {i*15} min")
-#        row[1].set_title(f"Predicted {i*15} min")
-#        row[2].set_title(f"observed - predicted {i*15} min")
-
-
-    plt.show()
 
 
 def plot_unet(args):
@@ -202,7 +111,6 @@ def plot_unet(args):
     mnwc = preprocess_many(read_times(times, producer='mnwc'), args.preprocess)
     cloudcast = infer_many(gt[0], len(times))
     initial = np.copy(gt[0])
-
     plot_timeseries([gt, cloudcast, mnwc], ['ground truth', 'cloudcast', 'mnwc'])
 
     mae_persistence = []
@@ -246,7 +154,6 @@ if CONVLSTM:
         # first two are used to create a prediction, and the latter one is
         # used to verify the prediction
 
-        print("Read ground truth")
         dataseries = read_images(times, image_series, args.preprocess)
         assert(len(dataseries) == (1 + history_len + prediction_len))
 
@@ -256,14 +163,12 @@ if CONVLSTM:
         future = np.asarray(datas[history_len+1:])
 
         # contains the predicted frames *only*
-        print("predict")
         predictions = predict_from_series(history, prediction_len)
         assert(predictions.shape[0] == prediction_len)
         assert(predictions.shape[0] == len(future))
 
         analysis_time = times[history_len+1]
 
-        print("read mnwc")
         mnwc = preprocess_many(read_times(times[history_len+1:], producer='mnwc', analysis_time=analysis_time.replace(minute=0)), args.preprocess)
 
         print(future.shape, predictions.shape, mnwc.shape)
