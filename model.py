2341d710997d51cf6b4f9fa5ff459d0ce01df367
Mikko Partio <mikko.partio@fmi.fi>
2021-11-14 09:18:06 +0200

    Set activation based on loss_function

--- model.py
+++ model.py
@@ -52,7 +52,9 @@ def unet(pretrained_weights=None, input_size=(256,256,1), loss_function='MeanSqu
     conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
     conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv10 = Conv2D(1, 1, activation = 'linear')(conv9)
+
+    activation = 'linear' if loss_function == 'logcosh' else 'sigmoid'
+    conv10 = Conv2D(1, 1, activation = activation)(conv9)
 
     model = Model(inputs = [inputs], outputs = [conv10])
 
