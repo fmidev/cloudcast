76389f169c5153597e51fe6641a80e99f36bedad
Mikko Partio <mikko.partio@fmi.fi>
2021-11-15 09:25:17 +0200

    Initial import

---
+++ verify-dataset.py
@@ -0,0 +1,57 @@
+import numpy as np
+import argparse
+from dateutil import parser as dateparser
+from datetime import datetime, timedelta
+import sys
+
+def parse_command_line():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", action='store')
+
+    args = parser.parse_args()
+
+#    args.start_date = datetime.datetime.strptime(args.start_date, '%Y-%m-%d')
+#    args.stop_date = datetime.datetime.strptime(args.stop_date, '%Y-%m-%d')
+
+    return args
+
+
+def verify(args):
+    dataset = np.load(args.filename)
+
+    print(dataset.files)
+    datas = dataset['arr_0']
+    times = dataset['arr_1']
+
+    assert(datas.shape[0] == times.shape[0])
+    print("datas and times length match: {}".format(datas.shape[0]))
+
+    static=0
+    miss=0
+    allmax=-1e38
+    allmin=1e38
+    
+    for i,arr in enumerate(datas):
+        min_ = np.min(arr)
+        max_ = np.max(arr)
+
+        if np.mean(arr) == 9999.0:
+            miss += 1
+        if min_ == max_:
+            static += 1
+        if allmax < max_:
+            allmax = max_
+        if allmin > min_:
+            allmin = min_
+
+        if min_ < -0.1: 
+            print('ERR: time {} min value: {}'.format(times[i], min_))
+            sys.exit(1)
+
+    print(f"found {miss} missing, {static} static grids out of {datas.shape[0]}")
+    print(f"max: {allmax}, min: {allmin}")
+
+if __name__ == "__main__":
+    args = parse_command_line()
+
+    verify(args)
