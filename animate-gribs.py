ae74cbb883a7b71dec47bb98c4ecbd95bf224890
Mikko Partio <mikko.partio@fmi.fi>
2022-02-23 13:39:30 +0200

    Extract plot labels from directory names (requires all directories to be located under /tmp)

--- animate-gribs.py
+++ animate-gribs.py
@@ -5,6 +5,15 @@ import glob
 import sys
 import subprocess
 from gributils import *
+from plotutils import reduce_label
+
+def extract_labels(directories):
+    labels = []
+    for d in directories:
+        labels.append(reduce_label(d.split('/')[2]))
+
+    return labels
+
 
 def read_gribs(directories):
     alldatas = []
@@ -24,32 +33,33 @@ def read_gribs(directories):
     return alldatas
 
 
-def save_anim(filename, datas):
+def save_anim(filename, datas, labels):
     rows = 1
     cols = len(datas)
     n_frames = len(datas[0])
 
     if cols > 2:
       rows = 2
-      cols = int(0.5 + cols / 2)
+      cols = int(0.5 + cols / rows)
  
     print(rows, cols)
-    fig, ax = plt.subplots(rows, cols,figsize=(16, 12))
-    models = ['unet bc','unet ssim','unet fss', 'ref']
+    fig, ax = plt.subplots(rows, cols,figsize=(18, 14))
+
     def update(j):
         for i in range(len(datas)):
             ax.flat[i].imshow(datas[i][j], cmap=cm.Greys_r)
-            ax.flat[i].set_title("{} {}/{}".format(models[i], j, n_frames), fontsize=20)
+            ax.flat[i].set_title("{} {}/{}".format(labels[i], j, n_frames), fontsize=16)
             ax.flat[i].set_axis_off()
 
-    anim = animation.FuncAnimation(fig, update, frames=np.arange(0, n_frames), interval=200)
+    anim = animation.FuncAnimation(fig, update, frames=np.arange(0, n_frames), interval=250, repeat_delay=500)
     anim.save(filename, dpi=80, writer='imagemagick')
     plt.close()
 
-    print(f"Saved {len(datas[0])} frame animation to file '{filename}'")
+    print(f"Saved {n_frames} frame animation to file '{filename}'")
 
 
 filename = sys.argv.pop()
 
+labels = extract_labels(sys.argv[1:])
 datas = read_gribs(sys.argv[1:])
-save_anim(filename, datas)
+save_anim(filename, datas, labels)
