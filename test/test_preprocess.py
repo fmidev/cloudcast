dec40a7d844bea1ba3a0b1a465b5ac6903be1258
Mikko Partio <mikko.partio@fmi.fi>
2023-02-22 19:48:31 +0200

    Add pca function, not finished yet

--- test/test_preprocess.py
+++ test/test_preprocess.py
@@ -3,39 +3,49 @@ import os
 import matplotlib.pyplot as plt
 from dateutil import parser
 from base.preprocess import *
+from base.gributils import read_grib
+
 
 @pytest.mark.skip(reason="plotting")
 def test_create_datetime():
-    da=[]
-    ya=[]
-    for d in np.arange(365*24):
-        #dt = datetime.datetime.strptime("2022-01-01", "%Y-%m-%d") + datetime.timedelta(days=int(d))
-        dt = parser.parse("2022-01-01 00:00:00 UTC") + datetime.timedelta(hours=(int(d)))
-        tod, toy = create_datetime(dt, (128,128))
+    da = []
+    ya = []
+    for d in np.arange(365 * 24):
+        # dt = datetime.datetime.strptime("2022-01-01", "%Y-%m-%d") + datetime.timedelta(days=int(d))
+        dt = parser.parse("2022-01-01 00:00:00 UTC") + datetime.timedelta(
+            hours=(int(d))
+        )
+        tod, toy = create_datetime(dt, (128, 128))
         print(dt, np.max(tod), np.max(toy))
-        assert(np.max(tod) == np.min(tod))
+        assert np.max(tod) == np.min(tod)
         da.append(np.max(tod))
         if dt.hour == 0:
             ya.append(np.max(toy))
 
     plt.subplot(2, 1, 1)
-    plt.plot(da[:49], label='Day')
+    plt.plot(da[:49], label="Day")
     plt.xlabel("Hour")
- 
+
     plt.subplot(2, 1, 2)
-    plt.plot(ya, label='Year')
-#    plt.legend(loc="upper left")
+    plt.plot(ya, label="Year")
+    #    plt.legend(loc="upper left")
     plt.xlabel("Julian day")
     plt.show()
-
-def test_create_datetime():
-    dt = parser.parse("2022-05-15 12:15:00 UTC")
-    tod, toy = create_datetime(dt, (3,3))
 
     assert(np.max(tod) == pytest.approx(-0.06540313))
     assert(np.max(tod) == np.min(tod))
     assert(np.max(toy) == pytest.approx(-0.6815403))
     assert(np.max(toy) == np.max(toy))
+
+def test_create_datetime():
+    dt = parser.parse("2022-05-15 12:15:00 UTC")
+    tod, toy = create_datetime(dt, (3, 3))
+
+    assert np.max(tod) == pytest.approx(-0.06540313)
+    assert np.max(tod) == np.min(tod)
+    assert np.max(toy) == pytest.approx(-0.6815403)
+    assert np.max(toy) == np.max(toy)
+
 
 def test_sun_elevation_angle():
     ts = parser.parse("2023-01-16 13:00:00+02")
@@ -43,7 +53,7 @@ def test_sun_elevation_angle():
     longitude = 25
     a = sun_elevation_angle_wrapper(ts, longitude, latitude)
     assert a == pytest.approx(8.73279)
-   
+
     ts = parser.parse("2019-04-10 19:00:00-06")
     latitude = 10
     longitude = -48
@@ -53,67 +63,93 @@ def test_sun_elevation_angle():
 
 def test_create_sun_elevation_angle():
 
-    ts = parser.parse("2022-05-16 00:00:00+00")
-    grid = create_sun_elevation_angle(ts, (128,128))
-    print(np.min(grid), np.max(grid))
+    ts = parser.parse("2022-06-30 00:00:00+00")
+    grid = create_sun_elevation_angle(ts, (128, 128))
     plt.subplot(2, 2, 1)
     plt.xlabel(ts)
     plt.imshow(np.squeeze(grid))
 
-    ts = parser.parse("2022-05-16 06:00:00+00")
-    grid = create_sun_elevation_angle(ts, (128,128))
-    print(np.min(grid), np.max(grid))
+    ts = parser.parse("2022-06-30 06:00:00+00")
+    grid = create_sun_elevation_angle(ts, (128, 128))
     plt.subplot(2, 2, 2)
     plt.xlabel(ts)
     plt.imshow(np.squeeze(grid))
 
-    ts = parser.parse("2022-05-16 12:00:00+00")
-    grid = create_sun_elevation_angle(ts, (128,128))
-    print(np.min(grid), np.max(grid))
+    ts = parser.parse("2022-06-30 12:00:00+00")
+    grid = create_sun_elevation_angle(ts, (128, 128))
     plt.subplot(2, 2, 3)
     plt.xlabel(ts)
     plt.imshow(np.squeeze(grid))
 
-    ts = parser.parse("2022-05-16 18:00:00+00")
-    grid = create_sun_elevation_angle(ts, (128,128))
-    print(np.min(grid), np.max(grid))
+    ts = parser.parse("2022-06-30 18:00:00+00")
+    grid = create_sun_elevation_angle(ts, (128, 128))
     plt.subplot(2, 2, 4)
     plt.xlabel(ts)
     plt.imshow(np.squeeze(grid))
 
-
     plt.show()
+
 
 def test_create_lonlat_grid():
-    grid = create_lonlat_grid((256,256))
+    grid = create_lonlat_grid((256, 256))
     plt.subplot(1, 2, 1)
     plt.xlabel("Longitude")
-    plt.imshow(np.squeeze(grid[...,0]))
+    plt.imshow(np.squeeze(grid[..., 0]))
     plt.subplot(1, 2, 2)
     plt.xlabel("Latitude")
-    plt.imshow(np.squeeze(grid[...,1]))
+    plt.imshow(np.squeeze(grid[..., 1]))
     plt.show()
 
-def test_create_leadtime():
-    onehot = create_onehot_leadtime_conditioning((3,3), 3, 1)
-    assert(onehot[0][0][0] == 0)
-    assert(onehot[1][0][0] == 1)
-    assert(onehot[2][0][0] == 0)
 
-    squeezed = create_squeezed_leadtime_conditioning((3,3), 12, 5)
-    assert(np.max(squeezed) == pytest.approx(0.41666666))
+def test_create_leadtime():
+    onehot = create_onehot_leadtime_conditioning((3, 3), 3, 1)
+    assert onehot[0][0][0] == 0
+    assert onehot[1][0][0] == 1
+    assert onehot[2][0][0] == 0
+
+    squeezed = create_squeezed_leadtime_conditioning((3, 3), 12, 5)
+    assert np.max(squeezed) == pytest.approx(0.41666666)
+
 
 def test_create_topo():
-    os.environ['S3_HOSTNAME'] = 'lake.fmi.fi'
-    os.environ['CLOUDCAST_INPUT_DIR'] = 's3://cc_archive'
+    os.environ["S3_HOSTNAME"] = "lake.fmi.fi"
+    os.environ["CLOUDCAST_INPUT_DIR"] = "s3://cc_archive"
 
     topo = create_topography_data("img_size=32x32")
-    assert(topo.shape == (32,32,1))
-    assert(np.max(topo) <= 1.0 and np.min(topo) >= 0.0)
+    assert topo.shape == (32, 32, 1)
+    assert np.max(topo) <= 1.0 and np.min(topo) >= 0.0
+
+
+def test_pca():
+    time = datetime.datetime.strptime("2020-05-10 12:00:00", "%Y-%m-%d %H:%M:%S")
+    data = read_grib(get_filename(time), img_size=(64, 64))
+    data = np.squeeze(data)
+
+    # Perform SVD
+    u, s, vt = np.linalg.svd(data)
+    # Create sigma matrix
+    if data.shape[0] == data.shape[1]:
+        sigma = np.diag(s)
+    else:
+        sigma = np.zeros((data.shape[0], data.shape[1]))
+        sigma[: data.shape[1], : data.shape[1]] = np.diag(s)
+
+    # pick n first components
+    n_elements = 6
+    sigma = sigma[:, :n_elements]
+    vt = vt[:n_elements, :]
+
+    # reconstruct matrix
+    B = u.dot(sigma.dot(vt))
+
+    plt.figure(1)
+    plt.imshow(data)
+    plt.figure(2)
+    plt.imshow(B)
+    plt.show()
 
 
 if __name__ == "__main__":
-    #test_create_lonlat_grid()
-    #test_sun_elevation_angle()
+    test_create_lonlat_grid()
     test_create_sun_elevation_angle()
-
+    #test_pca()
