bbdec4444a2e0b572bc415cccfc5d2ab16b909f2
Mikko Partio <mikko.partio@fmi.fi>
2026-04-07 08:35:20 +0300

    Read MAX_HOURS from environment and default to 8

--- inference/bin/convert.sh
+++ inference/bin/convert.sh
@@ -2,9 +2,10 @@ set -uex
 
 CC2_DATA_PATH="${CC2_DATA_PATH:-data}"
 CC2_ETC_PATH="${CC2_ETC_PATH:-etc}"
+MAX_HOURS="${MAX_HOURS:-8}"
 
-python3 python/prepare-data.py --producer nwcsaf $CC2_DATA_PATH/nwcsaf-surface.grib2 $CC2_DATA_PATH/nwcsaf-surface.nc
-python3 python/prepare-data.py --producer meps $CC2_DATA_PATH/meps-pressure.grib2 $CC2_DATA_PATH/meps-pressure.nc
+python3 python/prepare-data.py --producer nwcsaf --prediction-length $MAX_HOURS $CC2_DATA_PATH/nwcsaf-surface.grib2 $CC2_DATA_PATH/nwcsaf-surface.nc
+python3 python/prepare-data.py --producer meps --prediction-length $MAX_HOURS $CC2_DATA_PATH/meps-pressure.grib2 $CC2_DATA_PATH/meps-pressure.nc
 
 export atime_start=$(date -ud "${ANALYSIS_TIME}z 1 hours ago" +"%Y-%m-%d %H:00:00")
 export atime_end=$(date -ud "${ANALYSIS_TIME}z +$MAX_HOURS hours" +"%Y-%m-%d %H:00:00")
